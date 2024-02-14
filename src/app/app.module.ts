511ccb4c94d757e30650b6b22405511b6af8188e
Samantha Dejah Kitchener <106303147+DejahsDayDreams@users.noreply.github.com>
2024-02-14 11:23:37 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -10,11 +10,13 @@ import { AddTaskComponent } from './add-task/add-task.component';
   declarations: [
     AppComponent,
     TodoListComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
