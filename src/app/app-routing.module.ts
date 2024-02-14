9ed39cd0df2ab491849d7fc3be38a20f1c68f599
Samantha Dejah Kitchener <106303147+DejahsDayDreams@users.noreply.github.com>
2024-02-14 11:33:12 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/todo-list', pathMatch: 'full' }, // Redirect empty path to todo-list
+{path: 'todo-list', component: TodoListComponent} 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
